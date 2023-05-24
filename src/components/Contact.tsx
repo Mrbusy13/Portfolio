38d8e55710acc874d15795133b9d49d4dbf86134
Mrbusy <nickhughes13@googlemail.com>
2023-05-24 15:44:02 +0100

    contact page added with links to github, linkedin and an email address

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,24 @@
+import { AiOutlineGithub, AiOutlineLinkedin, AiOutlineMail } from "react-icons/ai"
 
 
 const Contact = () => {
+  const openInNewTab = (url:any) => {
+    window.open(url, "_blank", "noreferrer")
+  };
   return (
-    <div id="contact" className='h-screen w-full bg-orange-300'>
-        Contact
+    <div id="contact" className='h-screen w-full bg-slate-500 text-gray-100 '>
+        <div className="text-6xl md:text-8xl font-bold text-gray-100 p-5">
+        <h1>contact</h1>        
+        </div>
+        
+        <div className='min-h-full gap-10 flex flex-col items-center justify-center'>
+        <AiOutlineGithub onClick={()=>openInNewTab('https://github.com/Mrbusy13')} className="cursor-pointer hover:scale-110" size={100} />
+        <AiOutlineLinkedin onClick={()=>openInNewTab('https://www.linkedin.com/in/nick-hughes-7a5a428b/')} className="cursor-pointer hover:scale-110" size={100} />
+        <div className='flex flex-col items-center gap-2'>
+        <AiOutlineMail className="cursor-pointer" size={100}/><p>nickhughes13@googlemail.com</p>
+        </div>
+        </div>
+       
     </div>
   )
 }
